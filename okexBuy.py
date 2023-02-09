cc420a5691345767f0998e638e51d51f86e26bf2
Fang <>
2023-02-09 17:49:26 +0800

    --wip-- [skip ci]

--- okexBuy.py
+++ okexBuy.py
@@ -5,24 +5,13 @@ import requests
 import time
 from datetime import datetime, timedelta
 
-print("例子: python okexBuy.py BLUR \"2023-02-14 20:00:00\" 1 100\n\n")
+print("isolated: 逐仓\ncross: 全仓非保证金模式\ncash: 非保证金模式\n")
+tdmode = input("请输入账户交易模式: ")
 
-# 修改开始
-coin = "BTC"
-opentime = "2023-02-14 20:00:00"
-buy_price = 1
-buy_amount = 100
-# 修改结束
-
-if len(sys.argv) != 5:
-    print("输入参数不对, python okexBuy.py <币对(BTC)-> <交易USD数量(如500)> <开盘时间\"2023-01-01 00:00:00\"> <价格> <数量>")
-    sys.exit()
-else:
-    coin = sys.argv[1]
-    opentime = sys.argv[2]
-    opentime = opentime + ".000000"
-    buy_price = float(sys.argv[3])
-    buy_amount = float(sys.argv[4])
+coin = input("请输入交易币种(如 BLUR ): ")
+opentime = input("请输入开盘时间(2023-02-14 20:00:00): ")
+buy_price = float(input("请输入买入价格: "))
+buy_amount = float(input("请输入买入数量: "))
 
 print("OKEX 抢开盘...")
 print("买入:", coin)
@@ -43,7 +32,7 @@ def okexTrade(coin, okex, buy_price, buy_amount):
     pair = coin + "-USDT"
     request = {
         "instId": pair,
-        "tdMode": "cash",
+        "tdMode": tdMode,
         "side": "buy",
         "ordType": "limit",
         "px": buy_price,
@@ -56,7 +45,7 @@ def okexTrade(coin, okex, buy_price, buy_amount):
 if __name__ == '__main__':
     okexInit(config)
     while True:
-        timeB = datetime.strptime(opentime, "%Y-%m-%d %H:%M:%S.%f")
+        timeB = datetime.strptime(opentime, "%Y-%m-%d %H:%M:%S")
         timeA = datetime.now()
         if timeA > timeB:
             try:
@@ -66,7 +55,7 @@ if __name__ == '__main__':
                 print("抢购失败!", type(e).__name__, str(e))
                 pass
             timeC = datetime.now()
-            print("交易耗时", (timeC - timeA).microseconds, "微秒")
+            print("交易耗时", (timeC - timeA).microseconds/1000, "毫秒")
             break
         else:
             print("距离", timeB, "剩余时间:", (timeB - timeA), end = '\r')
