a1cb01a94a4bc05065077bba9176bfd430a12b97
Fang <>
2023-02-09 20:24:47 +0800

    --wip-- [skip ci]

--- binanceBuy.py
+++ binanceBuy.py
@@ -3,43 +3,31 @@ import json
 import sys
 import requests
 import time
-from datetime import datetime, timedelta
-
-print("isolated: 逐仓\ncross: 全仓非保证金模式\ncash: 非保证金模式\n")
-tdMode = input("请输入账户交易模式: ")
+from datetime import datetime
 
 coin = input("请输入交易币种(如 BLUR ): ")
 opentime = input("请输入开盘时间(2023-02-14 20:00:00): ")
 buy_price = float(input("请输入买入价格: "))
 buy_amount = float(input("请输入买入数量: "))
 
-print("\nOKEX 抢开盘...")
+print("\nBinance 抢开盘...")
 print("买入:", coin)
 print("价格:", buy_price)
 print("数量:", buy_amount)
 print("交易金额(USDT):", buy_price*buy_amount)
 
 config = json.load(open('config.json'))
-okex = {}
+binance = {}
 
 def exchangeInit(config):
-    global okex
-    okex["spot"] = ccxt.okx(config["okex"])
-    okex["spot"].load_markets()
+    global binance
+    binance["spot"] = ccxt.binance(config["binance"])
+    binance["spot"].load_markets()
 
-def exchangeTrade(coin, okex, buy_price, buy_amount):
-    #okex["spot"].load_markets()
-    pair = coin + "-USDT"
-    request = {
-        "instId": pair,
-        "tdMode": tdMode,
-        "side": "buy",
-        "ordType": "limit",
-        "px": buy_price,
-        "sz": buy_amount
-    }
-    okex["spot"].private_post_trade_order(request)
-    return request
+def exchangeTrade(coin, binance, buy_price, buy_amount):
+    pair = coin + "/USDT"
+    res = binance["spot"].createLimitBuyOrder(pair, buy_amount, buy_price)
+    return res
 
 
 if __name__ == '__main__':
@@ -49,7 +37,7 @@ if __name__ == '__main__':
         timeA = datetime.now()
         if timeA > timeB:
             try:
-                res = exchangeTrade(coin, okex, buy_price, buy_amount)
+                res = exchangeTrade(coin, binance, buy_price, buy_amount)
                 print("交易完成!", res)
             except Exception as e:
                 print("抢购失败!", type(e).__name__, str(e))
