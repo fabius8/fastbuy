d9de3197563bc616e050ebcd6771b1620eba1da8
Fang <>
2023-02-14 20:29:12 +0800

    --wip-- [skip ci]

--- okexBuy.py
+++ okexBuy.py
@@ -10,6 +10,11 @@ tdMode = input("请输入账户交易模式: ")
 
 coin = input("请输入交易币种(如 BLUR ): ")
 opentime = input("请输入开盘时间(2023-02-14 20:00:00): ")
+diff = input("请输入提前几毫秒发送报文(默认0):")
+if diff == "":
+    diff = 0
+else:
+    diff = int(diff)
 buy_price = float(input("请输入买入价格: "))
 buy_amount = float(input("请输入买入数量: "))
 
@@ -18,6 +23,7 @@ print("买入:", coin)
 print("价格:", buy_price)
 print("数量:", buy_amount)
 print("交易金额(USDT):", buy_price*buy_amount)
+print("提前多少时间买入(毫秒):", diff)
 
 config = json.load(open('config.json'))
 exchange = {}
@@ -46,7 +52,7 @@ if __name__ == '__main__':
     while True:
         timeB = datetime.strptime(opentime, "%Y-%m-%d %H:%M:%S")
         timeA = datetime.now()
-        if timeA > timeB:
+        if timeA - timedelta(microseconds=diff*1000) > timeB:
             try:
                 res = exchangeTrade(coin, exchange, buy_price, buy_amount)
                 print("交易完成!", res)
